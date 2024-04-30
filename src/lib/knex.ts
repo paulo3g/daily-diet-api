583be4c3f7ff1bd5abfbed46c8ddd05e4072d27a
paulo3g <paulo.programacao3625@gmail.com>
2024-04-30 00:02:10 -0300

    fix: Add missing environment variable to configuration

--- src/lib/knex.ts
+++ src/lib/knex.ts
@@ -2,7 +2,7 @@ import { Knex, knex as setupKnex } from 'knex'
 import { env } from '../env'
 
 export const config: Knex.Config = {
-  client: 'sqlite3',
+  client: env.DATABASE_CLIENT,
   connection: {
     filename: env.DATABASE_URL,
     timezone: 'UTC',
