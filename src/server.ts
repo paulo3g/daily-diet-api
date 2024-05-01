d1517df080d8ad438dc895efe7b32ae7dd0e3a8f
paulo3g <paulo.programacao3625@gmail.com>
2024-05-01 19:09:58 -0300

    feat: Set up testing environment

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,5 @@
-import fastify from 'fastify'
+import { app } from './app'
 import { env } from './env'
-import cookie from '@fastify/cookie'
-
-import { usersRoutes } from './routes/users'
-import { mealsRoutes } from './routes/meals'
-
-const app = fastify()
-
-app.register(cookie)
-
-app.register(usersRoutes, {
-  prefix: '/users',
-})
-
-app.register(mealsRoutes, {
-  prefix: '/meals',
-})
 
 app
   .listen({
